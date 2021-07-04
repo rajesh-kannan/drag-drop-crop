63d326dd04ac6fab5d00f786ab00dfbf56db0c1a
rajesh.kannan <rajesh.kannan@tavant.com>
2021-07-04 16:31:19 +0530

    fix: image-editor: adjust overlapping text issue

--- src/common/pipes/file-name/file-name.pipe.ts
+++ src/common/pipes/file-name/file-name.pipe.ts
@@ -8,7 +8,8 @@ export class FileNamePipe implements PipeTransform {
   transform(value: string, ...args: unknown[]): string {
     const idxOfExtensionStart = value.lastIndexOf('.');
     const actualFileName = value.slice(0, idxOfExtensionStart);
-    const extension = value.slice(idxOfExtensionStart + 1)
+    const extension = value.slice(idxOfExtensionStart + 1);
+
     const maxAllowedFileNameLength = args[0];
 
     const displayFileName = (actualFileName.length > maxAllowedFileNameLength)
